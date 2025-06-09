8c3b331ebd2d4effc8b6a91def72e7e6a642b5fe
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-06-09 23:23:31 +0200

    Feature: Basic guide npc and ColorModification shader created.

---
+++ Scripts/Characters/GuideScripts/Guide.cs
@@ -0,0 +1,32 @@
+using CarrotCottage.Scripts.Components;
+using Godot;
+using System;
+
+namespace CarrotCottage.Scripts.Characters.GuideScripts;
+
+public partial class Guide : Node2D
+{
+    private InteractableComponent _interactableComponent = default!;
+    private Control _interactableLabelComponent = default!;
+
+    public override void _Ready()
+    {
+        _interactableComponent = GetNode<InteractableComponent>(GuideConstants.Nodes.InteractableComponent);
+        _interactableLabelComponent = GetNode<Control>(GuideConstants.Nodes.InteractableLabelComponent);
+
+        _interactableComponent.InteractableActivated += OnInteractableActivated;
+        _interactableComponent.InteractableDeactivated += OnInteractableDeactivated;
+
+        _interactableLabelComponent.Visible = false;
+    }
+
+    private void OnInteractableActivated()
+    {
+        _interactableLabelComponent.Visible = true;
+    }
+
+    private void OnInteractableDeactivated()
+    {
+        _interactableLabelComponent.Visible = false;
+    }
+}
