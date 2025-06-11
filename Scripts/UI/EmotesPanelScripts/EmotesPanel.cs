d17b12168ea4d2e1d0ab672f92e3948ed1bb822d
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-06-11 12:35:04 +0200

    Feature: Emotes panel.

---
+++ Scripts/UI/EmotesPanelScripts/EmotesPanel.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System.Collections.Generic;
+
+namespace CarrotCottage.Scripts.UI.EmotesPanelScripts;
+
+public partial class EmotesPanel : Panel
+{
+    private AnimatedSprite2D _animatedSprite2D = default!;
+    private Timer _emoteIdleTimer = default!;
+
+    private readonly string[] _idleEmotes =
+    [
+        EmotesPanelConstants.Animations.Emote1Idle,
+        EmotesPanelConstants.Animations.Emote2Smile,
+        EmotesPanelConstants.Animations.Emote3EarWave,
+        EmotesPanelConstants.Animations.Emote4Blink
+    ];
+
+    public override void _Ready()
+    {
+        _animatedSprite2D = GetNode<AnimatedSprite2D>(EmotesPanelConstants.Nodes.AnimatedSprite2D);
+        _emoteIdleTimer = GetNode<Timer>(EmotesPanelConstants.Nodes.EmoteIdleTimer);
+
+        _animatedSprite2D.Play(EmotesPanelConstants.Animations.Emote1Idle);
+    }
+
+    public void OnEmoteIdleTimerTimeout()
+    {
+        var index = GD.RandRange(0, _idleEmotes.Length - 1);
+        var emoteName = _idleEmotes[index];
+
+        _animatedSprite2D.Play(emoteName);
+    }
+}
---
+++ Scripts/UI/EmotesPanelScripts/EmotesPanelConstants.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotCottage.Scripts.UI.EmotesPanelScripts;
+
+public static class EmotesPanelConstants
+{
+    public static class Nodes
+    {         
+        public const string AnimatedSprite2D = "Emote/AnimatedSprite2D";
+        public const string EmoteIdleTimer = "EmoteIdleTimer";
+    }
+
+    public static class Animations
+    {
+        public const string Emote1Idle = "Emote1Idle";
+        public const string Emote2Smile = "Emote2Smile";
+        public const string Emote3EarWave = "Emote3EarWave";
+        public const string Emote4Blink = "Emote4Blink";
+    }
+}
